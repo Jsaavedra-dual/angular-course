5146d3f2c0733b872e27d7abb80ffa3b9fe814d1
Jsaavedra-dual <jsaavedra@dualasiapacific.com>
2024-09-04 16:39:12 +1000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-course';
+
+  staticText = 'StaticText from Ts'; 
+
+  //Declaracion de objeto
+  ObjUserTest =  {
+    id: 3216,
+    name: 'Jair',
+    surname: 'Saavedra',
+    age: 19,
+    address: '1116654 Eltham',
+  };
+
+  onClick(){
+    console.log('clickXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+  }
+
+  onKeyUp(newDinamicText: string){
+    // console.log('hola mundo' + dinamicText);
+    this.ObjUserTest.name = newDinamicText;
+  }
+}
